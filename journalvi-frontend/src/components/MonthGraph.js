d6b20478107578218e41d481be5fcdefe2892be4
racross1 <racross1@gmail.com>
2021-03-05 11:01:04 -0800

    month average trend graph working and cleaned up

--- journalvi-frontend/src/components/MonthGraph.js
+++ journalvi-frontend/src/components/MonthGraph.js
@@ -1,235 +1,80 @@
-import React from "react";
-import Plot from "react-plotly.js";
-import {format, subMonths, addMonths, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, isSameMonth, isSameDay} from "date-fns";
-import { connect } from 'react-redux';
+import React from 'react';
+import Plot from 'react-plotly.js';
 
 
-// let currentMonth = this.props.currentMonth
-// let data = []
-// let layout = {}
 
-class MonthGraph extends React.Component {
+const MonthGraph = ({x, yPos, yNeg, yNeut, yMixed, currentMonthDisplay, revision}) => {
     
-    constructor(props) {
-        super(props);
-       
-        let xAxis = Object.keys(this.createXYAxisObj(this.props.selectedEntries, "POSITIVE"))
-        let yPosAxis = Object.values(this.createXYAxisObj(this.props.selectedEntries, "POSITIVE"))
-        let yNegAxis = Object.values(this.createXYAxisObj(this.props.selectedEntries, "NEGATIVE"))
-        let yNeutAxis =  Object.values(this.createXYAxisObj(this.props.selectedEntries, "NEUTRAL"))
 
-        let month = format(this.props.currentMonth, 'MMMM')
-        console.log(xAxis)
-        this.state = {
-            x: xAxis,
-            yPos: yPosAxis,
-            yNeg: yNegAxis,
-            yNeut: yNeutAxis,
-            currentMonthDisplay: month
-        }
-      
-    }
-
-
-
-createXYAxisObj= (entries, sentiment) => {
-     let dateFormat = "d"
-     let axisObj = {}
-     entries.forEach(entry => {
-        let dateNum = parseInt(format(this.parseEntryDate(entry.date), dateFormat))
-        axisObj[dateNum] = this.getScoreAverages(entry, sentiment)
-    })
-    //pop and push 1 and end of month if not included
-    //these will be the days of the week
-
-// console.log(dates.sort((a,b) => a - b))
-
-   return axisObj
-    // return dates.sort((a,b) => a - b)
-
-}
-
-getScoreAverages = (entry, sentiment) => {
-    //pos scores only rn
-    let total = 0;
-    console.log(entry.scores)
-    switch(sentiment) {
-        case 'POSITIVE':
-            entry.scores.forEach(score => {
-                total += score.pos_score
-            })
-            return total / entry.scores.length;
-           
-        case 'NEGATIVE':
-            entry.scores.forEach(score => {
-                total += score.neg_score
-            })
-            return total / entry.scores.length;
-
-        case 'NEUTRAL':
-            entry.scores.forEach(score => {
-                total += score.neut_score
-            })
-            return total / entry.scores.length;
-
-    }
-//    entry.scores.forEach(score => {
-//        total += score.pos_score
-//    })
-//    console.log(total)
-//     let avg = total / entry.scores.length;
-    
-   
-//     return avg
-}
-
-
-parseEntryDate = (date) => {
-    let d = new Date(date)
-    
-    return addDays(d, 1)
-
-
-}
-
-
-  render() {
-    console.log(this.props.selectedEntries)
     return (
-        <div style={{ width: "100%", height: "100%" }}>
+        <div style={{ width: '100%', height: '100%' }} id='trend-graph-month'>
         <Plot
             data={[
                 {
-                    x: this.state.x,
-                    y: this.state.yPos,
-                    type: "scatter",
-                    mode: "lines+markers",
-                    name: 'positive',
+                    x: x,
+                    y: yPos,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    name: 'Positive',
                     line: {
                         color: 'rgb(40,167,69)',
                         width: 3
                     }
                 },
                 {
-                    x: this.state.x,
-                    y: this.state.yNeg,
-                    type: "scatter",
-                    mode: "lines+markers",
-                    name: 'negative',
+                    x: x,
+                    y: yNeg,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    name: 'Negative',
                     line: {
                         color: 'rgb(220,53,69)',
                         width: 3
                     }
                 },
                 {
-                    x: this.state.x,
-                    y: this.state.yNeut,
-                    type: "scatter",
-                    mode: "lines+markers",
-                    name: 'neutral',
+                    x: x,
+                    y: yNeut,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    name: 'Neutral',
                     line: {
                         color: 'rgb(255,193,7)',
                         width: 3
                     }
-                }      
+                },
+                {
+                    x: x,
+                    y: yMixed,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    name: 'Mixed',
+                    line: {
+                        color: 'rgb(0,123,255)',
+                        width: 3
+                    }
+                }           
             ]}
             layout={{
-            width: 736,
-            height: 450,
-            paper_bgcolor: "rgba(0,0,0,0)",
-            plot_bgcolor: "rgba(0,0,0,0)",
-            title: `${this.state.currentMonthDisplay} Sentiment Trend`
+            width: 950,
+            height: 525,
+            yaxis: {
+                tickformat: ',.0%',
+                range: [0,1],
+                title: 'Average Entry Sentiment Score'
+            },
+            xaxis: {
+                title: 'Day'
+            },
+            title: `${currentMonthDisplay} Sentiment Trend`
             
             }}
-                    />
+            revision={revision}
+        />
        
       </div>
     );
   }
-}
 
-// const mapStateToProps = (state) => {
-    
-//     // console.log(state.timeReducer.currentMonth)
-    
-//     return {currentMonth: state.timeReducer.currentMonth}
-// }
-
-
-// export default connect(mapStateToProps)(MonthTrend);
 
 export default MonthGraph
-
-
-// prior code
-
-  // this.state = {
-        //     data: [
-        //       {
-        //         x:[1,31],
-        //         y: [3, 5, 1, 7],
-               
-        //         type: 'scatter',
-        //         name: 'Vendor'
-        //       },
-        //       {
-        //         // x: this.createXAxisArr(this.props.entries),
-        //         x:[1,31],
-        //         y: [3, 5, 1, 7],
-               
-        //         type: 'scatter',
-        //         name: 'Provider'
-        //       }],
-        //     layout: {
-        //       height: 400,
-        //       width: 600,
-        //       title: "Area chart"
-        //     }
-        //   };
-
-        // componentDidUpdate(){
-//     //do version of this on component did mount too
-//         if(this.state.currentMonth !== this.props.currentMonth){ 
-//             // this.setState({currentMonth: this.props.currentMonth})
-//         //     let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), this.props.currentMonth))
-//         //     let xAxis = this.createXAxisArr(selectedEntries)
-//         //     this.setState({data: {
-//         //         ...this.state.data, 
-//         //         x: xAxis,
-//         //     }
-//         // })
-//          }
-//      }
-
-// componentDidMount(){
-//     this.setState({
-//         x: Object.keys(this.createXYAxisObj(this.props.selectedEntries)),
-//         y: Object.values(this.createXYAxisObj(this.props.selectedEntries)),
-//     })
-// }
-    
-    
-    // let currentMonth = this.props.currentMonth
-    // // this.setState({currentMonth: currentMonth})
-
-    // let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), this.props.currentMonth))
-    // this.createXAxisArr(selectedEntries)
-    
-    // console.log('yes')
-    // console.log(selectedEntries)
-
-    // let monthStart = startOfMonth(this.props.currentMonth);
-    // let monthEnd = endOfMonth(this.props.currentMonth);
-
-    // let dateFormat = "d"
-
-    // let formattedStart = format(monthStart, dateFormat)
-    // let formattedEnd = format(monthEnd, dateFormat)
-    // this.createXYAxisObj(selectedEntries)
-   
-// }
-
-// componentDidUpdate(){
-//     console.log('update')
-
-// }
--- journalvi-frontend/src/containers/Dashboard.js
+++ journalvi-frontend/src/containers/Dashboard.js
@@ -2,7 +2,6 @@ import React from "react";
 import Calendar from '../components/Calendar.js'
 import { connect } from 'react-redux';
 import PieChart from "../components/PieChart.js"
-import MonthTrend from "../components/MonthTrend.js"
 import MonthGraph from "../components/MonthGraph.js"
 import {format, subMonths, addMonths, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, isSameMonth, isSameDay} from "date-fns";
 
@@ -10,33 +9,98 @@ import {format, subMonths, addMonths, startOfMonth, endOfMonth, startOfWeek, end
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-        let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), new Date()))
+        let initMonth = new Date()
+        let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), initMonth))
+        let xAxis = Object.keys(this.createXYAxisObj(selectedEntries, "POSITIVE"))
+        let yPosAxis = Object.values(this.createXYAxisObj(selectedEntries, "POSITIVE"))
+        let yNegAxis = Object.values(this.createXYAxisObj(selectedEntries, "NEGATIVE"))
+        let yNeutAxis =  Object.values(this.createXYAxisObj(selectedEntries, "NEUTRAL"))
+        let yMixedAxis = Object.values(this.createXYAxisObj(selectedEntries, "MIXED"))
+        let monthDisplay = format(initMonth,'MMMM')
+
     this.state = {
         currentMonth: new Date(),
-        selectedEntries: selectedEntries
+        selectedEntries: selectedEntries,
+        revision: 0,
+        x: xAxis,
+        yPos: yPosAxis,
+        yNeg: yNegAxis,
+        yNeut: yNeutAxis,
+        yMixed: yMixedAxis,
+        currentMonthDisplay: monthDisplay
     }}
 
     setMonth = (month) => {
         let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), month))
+        let revisionPlusOne = this.state.revision + 1
+        let xAxis = Object.keys(this.createXYAxisObj(selectedEntries, "POSITIVE"))
+        let yPosAxis = Object.values(this.createXYAxisObj(selectedEntries, "POSITIVE"))
+        let yNegAxis = Object.values(this.createXYAxisObj(selectedEntries, "NEGATIVE"))
+        let yNeutAxis =  Object.values(this.createXYAxisObj(selectedEntries, "NEUTRAL"))
+        let yMixedAxis = Object.values(this.createXYAxisObj(selectedEntries, "MIXED"))
+        let monthDisplay = format(month,'MMMM')
+        
         this.setState({
             currentMonth: month,
-            selectedEntries: selectedEntries
+            selectedEntries: selectedEntries,
+            revision: revisionPlusOne,
+            x: xAxis,
+            yPos: yPosAxis,
+            yNeg: yNegAxis,
+            yNeut: yNeutAxis,
+            yMixed: yMixedAxis,
+            currentMonthDisplay: monthDisplay 
         })
     }
 
-    selectEntries = (entries) => {
-        let selectedEntries = this.props.entries.filter(entry => isSameMonth(this.parseEntryDate(entry.date), this.state.currentMonth))
-        return selectedEntries
+    createXYAxisObj= (entries, sentiment) => {
+        let dateFormat = "d"
+        let axisObj = {}
+        entries.forEach(entry => {
+           let dateNum = parseInt(format(this.parseEntryDate(entry.date), dateFormat))
+           axisObj[dateNum] = this.getScoreAverages(entry, sentiment)
+       })
+
+      return axisObj
+   
+   }
+
+   getScoreAverages = (entry, sentiment) => {
+    let total = 0;
+    switch(sentiment) {
+        case 'POSITIVE':
+            entry.scores.forEach(score => {
+                total += score.pos_score
+            })
+            return total / entry.scores.length;
+           
+        case 'NEGATIVE':
+            entry.scores.forEach(score => {
+                total += score.neg_score
+            })
+            return total / entry.scores.length;
+
+        case 'NEUTRAL':
+            entry.scores.forEach(score => {
+                total += score.neut_score
+            })
+            return total / entry.scores.length;
+        case 'MIXED':
+            entry.scores.forEach(score => {
+                total += score.mixed_score
+            })
+            return total / entry.scores.length;
 
     }
+}
 
-    parseEntryDate = (date) => {
-        let d = new Date(date)
-        
-        return addDays(d, 1)
+parseEntryDate = (date) => {
+    let d = new Date(date)
     
-    
-    }
+    return addDays(d, 1)
+
+}
+
     
 
 render(){
@@ -45,10 +109,20 @@ render(){
         <div id='dashboard'>
             <Calendar entries={this.props.entries} setMonth={this.setMonth}/>
             {/* <PieChart entries={this.props.entries}/> */}
-            <MonthGraph entries={this.props.entries} selectedEntries={this.state.selectedEntries} currentMonth={this.state.currentMonth}/>
+            <MonthGraph 
+            entries={this.props.entries} 
+            selectedEntries={this.state.selectedEntries} 
+            currentMonth={this.state.currentMonth}
+            revision={this.state.revision}
+            x={this.state.x}
+            yPos={this.state.yPos}
+            yNeg={this.state.yNeg}
+            yNeut={this.state.yNeut}
+            yMixed={this.state.yMixed}
+            currentMonthDisplay={this.state.currentMonthDisplay}/>
         </div>
-    )
-}
+        )
+    }
 }
 
 function mapStateToProps (state) {
